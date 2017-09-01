85b0d48d816ce58a8ba4ed48a651e934f6045714
zzq369 <123456aa>
2017-09-01 21:19:07 +0800

    抓取示例

--- backend/app/grab/controller/IndexController.php
+++ backend/app/grab/controller/IndexController.php
@@ -6,36 +6,37 @@ namespace app\grab\controller;
 
 use cmf\controller\BaseController;
 use think\Db;
-use Curl\Curl;
 
 class IndexController extends BaseController
 {
 
     public function index()
     {
-        $url = 'http://www.bdwork.com/forum-40-1.html';
+        $url = 'http://www.bdwork.com/thread-69731-1-1.html';
         $url = trim($url);
         if(!strstr($url,'https://') && !strstr($url,'http://')){
             $url = 'https://' . $url;
         }
-        $curl = curl_init();
-        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
-        curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,false);
-        curl_setopt($curl,CURLOPT_HTTPHEADER,'\'user-agent\', \'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36\'');
-        /curl_setopt($curl,CURLOPT_HTTPHEADER,'\'accept-language\',\'zh-CN,zh;q=0.8,en;q=0.6\'');
-        $response = curl_exec($curl);
-        $data = curl_getinfo($curl);
-        print_r($data);die;
-        curl_close($curl);
-        if(!$data){
+
+        vendor('simple_html_dom');
+        $html = file_get_html($url);
+        $title = $html->find('#thread_subject',0);
+        $t = $title->plaintext;
+        $t = iconv("UTF-8", "GB2312//IGNORE", $t) ;
+        echo $t;die;
+        foreach($title as $val){
+            echo $val->plaintext;
+        }
+        die;
+        dump($title);die;
+
+        //获取所有的img元素
+        foreach($html->find('img') as $element)
+            echo $element->src . '<br>';
+        die;
+        if(!$html){
            echo "不存在页面";die;
         }
-        vendor('simple_html_dom');
-        $document = str_get_html($data);
 
-        $rs = array();
-
-        //标题title
-        $title = $document->find('.top .top_nav strong', 0);
     }
 }
