ea3fae3948e132e4714b4b7af29eab41c75a1546
zzq369 <123456aa>
2017-09-03 21:18:14 +0800

    抓取代码

--- backend/app/grab/controller/AdController.php
+++ backend/app/grab/controller/AdController.php
@@ -5,10 +5,11 @@
  */
 namespace app\grab\controller;
 
-use app\grab\model\AdCategoryModel;
-use app\grab\model\AdModel;
+use app\grab\model\PushCategoryModel;
+use app\grab\model\PushModel;
 use cmf\controller\BaseController;
 use think\Db;
+use think\Exception;
 
 class AdController extends BaseController
 {
@@ -19,88 +20,27 @@ class AdController extends BaseController
     public function run(){
         set_time_limit(0);
         ini_set("memory_limit", '128M');
-        $url = 'http://www.admaimai.com/shop/l5a0c0kp1.html';
-        if(empty($url)){
-            echo "请输入URL";
-            die;
-        }
-        echo $url."<br />";
-        ob_flush();
-        flush();
-        vendor('simple_html_dom');
-        $html = file_get_html($url);
-        $comList = $html->find(".sh16_wid02");
-        foreach($comList as $key=>$dom){
-            echo "run compan list ----" . $key .'<br>';
-            ob_flush();
-            flush();
-            $href_list = $dom->find(".sh16_m02_1 .sh16_m02_2 .sh16_lf a", 0)->href;
-            $url_list = 'http://www.admaimai.com'.$href_list;
-            echo "run company info----" . $url_list .'<br>';
-            ob_flush();
-            flush();
-            $html = file_get_html($url_list);
-            $adList = $html->find(".zxt_04f2");
-            foreach($adList as $key=>$dom){
-                if($key == 1) continue;
-                echo "run ad list----" . $key .'<br>';
-                ob_flush();
-                flush();
-                $href_ad = $dom->find(".z_zy_v3 .z_f3", 0)->href;
-                echo "run ad href----" . $href_ad .'<br>';
-
-                $url_ad = 'http://www.admaimai.com'.$href_ad;
-                echo "----" . $url_ad .'<br>';
-                ob_flush();
-                flush();
-                echo "info ----4 <br>";
-                $html = file_get_html($url_ad);
-                echo "info ----1 <br>";
-                //公司名称
-                $company_name = $html->find(".zxt_01 .z_f3", 1);
-                $company_name = $company_name->plaintext;
-                echo "info ----2<br>";
-                //标题
-                $title = $html->find(".zxt_04f1_zy",0);
-                $title = $title->plaintext;
-                echo "info ----3<br>";
-                if(empty($title)){
-                    continue;
-                }
-                //地区
-                $provice = $html->find(".t_lf .z_f3",0);
-                $provice = $provice->plaintext;
-                $city = $html->find(".t_lf .z_f3",1);
-                $city = $city->plaintext;
-                $address = $provice . " " .$city;
-
-                //类型
-                $type_tr = $html->find(".z_h_12b1 tr",1);
-                $type_list = $type_tr->find(".z_f3");
-                $type = [];
-                foreach($type_list as $val){
-                    $type_one = $val->plaintext;
-                    $type[] = $type_one;
-                }
-                $type = implode(",", $type);
-
-                //电话
-                $tel_info = $html->find(".z_h_12g .z_f5", 0);
-                $tel = $tel_info->plaintext;
-                //内容
-                $content_info = $html->find(".zxt_04f3_zy",0);
-                $content = $content_info->innertext;
-                $data = [];
-                $data['title'] = $title;
-                $data['company_name'] = $company_name;
-                $data['type'] = $type;
-                $data['tel'] = $tel;
-                $data['address'] = $address;
-                $data['content'] = $content;
-                $adModel = new AdModel();
-                $adModel->addAd($data);
-                unset($html, $title, $company_name, $type, $tel, $address, $content);
+        for($p = 1; $p<=70; $p++){
+            $url = 'http://www.admaimai.com/shop/l5a0c0kp'.$p.'.html';
+            if(empty($url)){
+                echo "请输入URL";
+                die;
             }
+            echo $url."<br />";
+            ob_flush();
+            flush();
+            vendor('simple_html_dom');
+            $html = file_get_html($url);
+            $comList = $html->find(".sh16_wid02 .sh16_m02_1");
+            foreach($comList as $key=>$dom){
+                echo "run compan list ----" . $key .'<br>';
+                ob_flush();
+                flush();
+                $href_list = $dom->find(".sh16_m02_2 .sh16_lf a", 0)->href;
+                $this->runList($href_list);
+                unset($href_list);
+            }
+            unset($comList,$html,$url);
         }
     }
 
@@ -112,19 +52,21 @@ class AdController extends BaseController
         $html = file_get_html($url);
         $adList = $html->find(".zxt_04f2");
         foreach($adList as $key=>$dom){
+            //判断是否可抓取内容
             echo "run ad list----" . $key .'<br>';
             ob_flush();
             flush();
-            $href = $dom->find(".z_zy_v3 .z_f3", 0)->href;
-            echo "run ad href----" . $href .'<br>';
-            $this->adInfo($href);
+            $href_ad = $dom->find(".z_zy_v3 .z_f3", 0)->href;
+            echo "run ad href----" . $href_ad .'<br>';
+            $this->adInfo($href_ad);
+            unset($href_ad, $dom);
         }
-
+        unset($adList, $html, $url);
     }
 
     public function runCategory(){
         $arr = ['户外','电视','广播','报纸','杂志','制作','策划','设备','材料','新兴'];
-        $adcModel = new AdCategoryModel();
+        $adcModel = new PushCategoryModel();
         $data = [];
         foreach($arr as $v){
             $data[] = array(
@@ -137,56 +79,65 @@ class AdController extends BaseController
     }
 
     public function adInfo($url){
-        $url = 'http://www.admaimai.com'.$url;
-        echo "----" . $url .'<br>';
-        ob_flush();
-        flush();
-        echo "info ----4 <br>";
-        $html = file_get_html($url);
-        echo "info ----1 <br>";
-        //公司名称
-        $company_name = $html->find(".zxt_01 .z_f3", 1);
-        $company_name = $company_name->plaintext;
-        echo "info ----2<br>";
-        //标题
-        $title = $html->find(".zxt_04f1_zy",0);
-        $title = $title->plaintext;
-        echo "info ----3<br>";
-        if(empty($title)){
-            return false;
-        }
-        //地区
-        $provice = $html->find(".t_lf .z_f3",0);
-        $provice = $provice->plaintext;
-        $city = $html->find(".t_lf .z_f3",1);
-        $city = $city->plaintext;
-        $address = $provice . " " .$city;
+        try {
+            $url = 'http://www.admaimai.com' . $url;
+            echo "----" . $url . '<br>';
+            ob_flush();
+            flush();
+            $html = file_get_html($url);
+            $can_push = $html->find(".zxt_03b", 0);
+            $attr = $can_push->attr;
+            if (!isset($attr['style'])) {
+                return false;
+            }
 
-        //类型
-        $type_tr = $html->find(".z_h_12b1 tr",1);
-        $type_list = $type_tr->find(".z_f3");
-        $type = [];
-        foreach($type_list as $val){
-            $type_one = $val->plaintext;
-            $type[] = $type_one;
-        }
-        $type = implode(",", $type);
+            //公司名称
+            $company_name = $html->find(".zxt_01 .z_f3", 1);
+            $company_name = $company_name->plaintext;
+            //标题
+            $title = $html->find(".zxt_04f1_zy", 0);
+            $title = $title->plaintext;
+            if (empty($title)) {
+                return false;
+            }
+            //地区
+            $provice = $html->find(".t_lf .z_f3", 0);
+            $provice = $provice->plaintext;
+            $city = $html->find(".t_lf .z_f3", 1);
+            $city = $city->plaintext;
+            $address = $provice . " " . $city;
 
-        //电话
-        $tel_info = $html->find(".z_h_12g .z_f5", 0);
-        $tel = $tel_info->plaintext;
-        //内容
-        $content_info = $html->find(".zxt_04f3_zy",0);
-        $content = $content_info->innertext;
-        $data = [];
-        $data['title'] = $title;
-        $data['company_name'] = $company_name;
-        $data['type'] = $type;
-        $data['tel'] = $tel;
-        $data['address'] = $address;
-        $data['content'] = $content;
-        $adModel = new AdModel();
-        $adModel->addAd($data);
-        unset($html, $title, $company_name, $type, $tel, $address, $content);
+            //类型
+            $type_tr = $html->find(".z_h_12b1 tr", 1);
+            $type_list = $type_tr->find(".z_f3");
+            $type = [];
+            foreach ($type_list as $val) {
+                $type_one = $val->plaintext;
+                $type[] = $type_one;
+            }
+            $type = implode(",", $type);
+
+            //电话
+            $tel_info = $html->find(".z_h_12g .z_f5", 0);
+            $tel = $tel_info->plaintext;
+            //内容
+            $content_info = $html->find(".zxt_04f3_zy", 0);
+            $content = $content_info->innertext;
+            $data = [];
+            $data['user_id'] = 2;
+            $data['title'] = $title;
+            $data['company_name'] = $company_name;
+            $data['type'] = $type;
+            $data['tel'] = $tel;
+            $data['address'] = $address;
+            $data['business_offer'] = $content;
+            $data['category_id'] = 1;
+            $pushModel = new PushModel();
+            $pushModel->insertGetId($data);
+            unset($html, $title, $company_name, $type, $tel, $address, $content);
+            return true;
+        }catch (Exception $e){
+            return true;
+        }
     }
 }
---
+++ sql/ht_push.sql
@@ -0,0 +1,43 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50547
+Source Host           : localhost:3306
+Source Database       : ht
+
+Target Server Type    : MYSQL
+Target Server Version : 50547
+File Encoding         : 65001
+
+Date: 2017-09-03 20:24:44
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for ht_push
+-- ----------------------------
+DROP TABLE IF EXISTS `ht_push`;
+CREATE TABLE `ht_push` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `title` varchar(200) DEFAULT NULL COMMENT '标题',
+  `company_name` varchar(100) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL COMMENT '开始时间',
+  `end_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `read_count` int(11) DEFAULT '0' COMMENT '阅读数',
+  `is_top` tinyint(1) DEFAULT '0' COMMENT '是否置顶  0 否 1是',
+  `is_anxious` tinyint(1) DEFAULT '0' COMMENT ' 是否急 0否  1 是',
+  `category_id` int(11) DEFAULT NULL COMMENT '资源类型',
+  `charge` varchar(50) DEFAULT NULL COMMENT '收费方式',
+  `business_offer` text COMMENT '商家提供',
+  `support` text COMMENT '支持',
+  `tel` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COMMENT='商家互推表';
+SET FOREIGN_KEY_CHECKS=1;
