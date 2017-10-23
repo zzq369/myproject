f20a71c9371cf4195904b4697f17075f2f9b9350
xy <zzq369@github.com>
2017-10-23 23:19:10 +0800

    后台互推标签js修改

--- backend/data/conf/route.php
+++ backend/data/conf/route.php
@@ -1,2 +1,25 @@
 <?php	return array (
+  '运营推广/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  '运营推广' => 
+  array (
+    0 => 'portal/List/index?id=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
 );
